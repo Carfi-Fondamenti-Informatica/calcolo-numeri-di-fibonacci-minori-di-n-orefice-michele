2fc40cc6d57e32efbc85fc87e37d273e0287c0d9
orefice-michele <91534750+orefice-michele@users.noreply.github.com>
2021-11-13 12:49:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+   int i=0, suc=1, prec=0, ris=0, n=0;
+    cin>>n;
+    prec=i;
+    if (n!=0){
+        cout<<"1"<<endl;
+    }
+    for (int i = 1; i <=n ; i++) {
+        ris=(prec+suc);
+        prec=suc;
+        suc=ris;
+        if (ris<=n){
+            cout<<ris<<endl;
+        }
+        
+    }
    return 0;
 }
